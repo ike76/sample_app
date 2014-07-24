be07e10b263355d916fd7f7a4f6ee3967d0e05e3
Brian <ike76@me.com>
2014-07-24 13:31:00 -0700

    finish static pages yo

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  config.include Capybara::DSL
+
 end
+
+# This file is copied to spec/ when you run 'rails generate rspec:install'
